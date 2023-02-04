ad893dcc1a2c639614167db79e9c6c04ce767bba
Alec S <asummers34@gmail.com>
2023-02-03 22:18:17 -0700

    updating the index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,2 +1,18 @@
-let time = dayjs().hour();
-console.log(time);
+var day = $('#currentDay');
+var time = $('.timeblock');
+var timeBlock = $('.hour');
+console.log(timeBlock);
+console.log(time.text())
+
+function dayStamp() {
+    var date = dayjs().day(0);
+    var formatDate = dayjs(date.$d).format('dddd, MMMM DD');
+    day.text(formatDate);
+}
+dayStamp();
+console.log(dayjs().hour());
+$.each(time, function(){
+    if (time.text()==dayjs().hour()) {
+        timeBlock.addClass('present');
+    }
+})

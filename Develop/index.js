5ace4ec4de3d885ff8f515f59d4b6bfd3c39b5fe
Alec S <asummers34@gmail.com>
2023-02-08 15:00:53 -0700

    adding Readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,123 +1,122 @@
-var day = $('#currentDay');
-var button1 = $('#button1');
-var button2 = $('#button2');
-var button3 = $('#button3');
-var button4 = $('#button4');
-var button5 = $('#button5');
-var button6 = $('#button6');
-var button7 = $('#button7');
-var button8 = $('#button8');
-var button9 = $('#button9');
-var confirmSave = $('.container');
-var textBlock = $('textarea');
-var $form1 = $('#input-9');
-var $form2 = $('#input-10');
-var $form3 = $('#input-11');
-var $form4 = $('#input-12');
-var $form5 = $('#input-13');
-var $form6 = $('#input-14');
-var $form7 = $('#input-15');
-var $form8 = $('#input-16');
-var $form9 = $('#input-17');
+var day = $("#currentDay");
+var button1 = $("#button1");
+var button2 = $("#button2");
+var button3 = $("#button3");
+var button4 = $("#button4");
+var button5 = $("#button5");
+var button6 = $("#button6");
+var button7 = $("#button7");
+var button8 = $("#button8");
+var button9 = $("#button9");
+var confirmSave = $(".container");
+var textBlock = $("textarea");
+var $form1 = $("#input-9");
+var $form2 = $("#input-10");
+var $form3 = $("#input-11");
+var $form4 = $("#input-12");
+var $form5 = $("#input-13");
+var $form6 = $("#input-14");
+var $form7 = $("#input-15");
+var $form8 = $("#input-16");
+var $form9 = $("#input-17");
 var currentTime = dayjs().hour();
 
-
 function dayStamp() {
-    var date = dayjs();
-    var formatDate = dayjs(date.$d).format('dddd, MMMM DD');
-    day.text(formatDate);
+  var date = dayjs();
+  var formatDate = dayjs(date.$d).format("dddd, MMMM DD");
+  day.text(formatDate);
 }
 dayStamp();
 
-button1.on('click', function(){
-    var input = $form1.val()
-    localStorage.setItem("form1", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button1.on("click", function () {
+  var input = $form1.val();
+  localStorage.setItem("form1", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button2.on('click', function(){
-    var input = $form2.val()
-    localStorage.setItem("form2", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button2.on("click", function () {
+  var input = $form2.val();
+  localStorage.setItem("form2", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button3.on('click', function(){
-    var input = $form3.val()
-    localStorage.setItem("form3", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button3.on("click", function () {
+  var input = $form3.val();
+  localStorage.setItem("form3", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button4.on('click', function(){
-    var input = $form4.val()
-    localStorage.setItem("form4", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button4.on("click", function () {
+  var input = $form4.val();
+  localStorage.setItem("form4", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button5.on('click', function(){
-    var input = $form5.val()
-    localStorage.setItem("form5", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button5.on("click", function () {
+  var input = $form5.val();
+  localStorage.setItem("form5", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button6.on('click', function(){
-    var input = $form6.val()
-    localStorage.setItem("form6", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button6.on("click", function () {
+  var input = $form6.val();
+  localStorage.setItem("form6", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button7.on('click', function(){
-    var input = $form7.val()
-    localStorage.setItem("form7", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button7.on("click", function () {
+  var input = $form7.val();
+  localStorage.setItem("form7", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button8.on('click', function(){
-    var input = $form8.val()
-    localStorage.setItem("form8", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button8.on("click", function () {
+  var input = $form8.val();
+  localStorage.setItem("form8", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
-button9.on('click', function(){
-    var input = $form9.val()
-    localStorage.setItem("form9", input);
-    var message = $('<p>Saved to local Storage ☑️</p>');
-    message.addClass('text-center');
-    confirmSave.prepend(message)
-    setTimeout(() => {
-        message.remove(); 
-    },500);
+button9.on("click", function () {
+  var input = $form9.val();
+  localStorage.setItem("form9", input);
+  var message = $("<p>Saved to local Storage ☑️</p>");
+  message.addClass("text-center");
+  confirmSave.prepend(message);
+  setTimeout(() => {
+    message.remove();
+  }, 500);
 });
 
 var response1 = localStorage.getItem("form1");
@@ -139,13 +138,13 @@ $form7.text(response7);
 $form8.text(response8);
 $form9.text(response9);
 
-textBlock.each(function(){
-    var blockTime = $(this).attr('id').split('-')[1]
-    if (blockTime<currentTime){
-        $(this).addClass('past');
-    } else if (blockTime==currentTime) {
-        $(this).addClass('present');
-    } else {
-        $(this).addClass('future');
-    }
-})
+textBlock.each(function () {
+  var blockTime = $(this).attr("id").split("-")[1];
+  if (blockTime < currentTime) {
+    $(this).addClass("past");
+  } else if (blockTime == currentTime) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
---
+++ README.md
@@ -0,0 +1,45 @@
+
+# Ch.05-bootstrap-calander 
+
+
+## Description
+This is a daily planner calander. Using bootstrap and jquery to implement the design and functionality of the challenge. The goal being that the textarea is dynamic with the time of the day. And use of planning the course of your day. 
+
+## Table of Contents
+- [Ch.05-bootstrap-calander](#ch05-bootstrap-calander)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Screenshot](#screenshot)
+  - [Deployed Link](#deployed-link)
+  - [Questions](#questions)
+
+## Installation
+none
+
+## Usage
+No specifics required
+
+## License
+none
+
+## Contributing
+No contributing allowed
+
+## Tests
+no tests required
+
+## Screenshot 
+![Screenshot of challenge](./Develop/Screen%20Shot%202023-02-08%20at%202.57.08%20PM.png)
+
+## Deployed Link
+https://asummers7.github.io/Ch.05-bootstrap-calander/
+
+## Questions
+If you have any questions about the project you can email me directly at asummers34@gmail.com. Or you can visit
+my Github repository at [asummers7](https://www.github.com/asummers7) 
+
